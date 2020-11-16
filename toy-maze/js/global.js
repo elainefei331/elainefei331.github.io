17fc6f717778d04b6cefbcc57409932226055c44
elainefei331 <feie482@newschool.edu>
2020-11-16 21:44:26 +1000

    1

---
+++ toy-maze/js/global.js
@@ -0,0 +1,13 @@
+window.addEventListener("load",function() {
+    var blueSquares = document.querySelectorAll('.blue');
+    
+    blueSquares.forEach(function(blue) {
+        //do something with the individual dark blue square
+        blue.addEventListener('mouseover', onHoverOverWalls);
+    });
+
+    function onHoverOverWalls() {
+        //what you want to happen when someone hits the walls
+    }
+
+})
