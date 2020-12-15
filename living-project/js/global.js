07a6905c937ead66d41243b16fd06b7b2b4320d1
elainefei331 <feie482@newschool.edu>
2020-12-15 22:42:48 +1000

    1

--- living-project/js/global.js
+++ living-project/js/global.js
@@ -87,3 +87,20 @@ function prev() {
     nextItem.classList.remove("carousel-item-prev");
   }, 600)
 }
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
